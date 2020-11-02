858157a18877812568a5397316e424d5bf159b28
Jim Wilkerson <jwilker2450@gmail.com>
2020-11-02 06:17:42 -0700

    added filtering, cleaned up paging, did some cleanup

--- restaurants/src/components/Finder.js
+++ restaurants/src/components/Finder.js
@@ -8,30 +8,33 @@ class Finder extends React.Component {
     this.state = {
       data: undefined,
       currentPage: 1,
-      maxItems: 10
+      maxItems: 10,
+      searchText:"",
+      selectedState:"All",
+      selectedGenre:"All"
     };
   }
 
   render() {
+
     var content = this.state.data;
     var currentPage = this.state.currentPage;
     var maxItems = this.state.maxItems;
-    var callback = this.setPage.bind(this);
     var pageOffset = currentPage - 1;
-    var pageStart = (pageOffset * maxItems) + 1;
+    var pageStart = (pageOffset * maxItems);
+
+    var setPageCallback = this.setPage.bind(this);
+    var searchCallback = this.search.bind(this);
 
     if (content !== undefined && content.data.length > 0) {
 
-      var items =
-        this.sortData(content.data).slice(pageStart, pageStart + maxItems);
+      var stateOptionsList = this.getStates(content.data);
+      var genreOptionsList = this.getGenres(content.data);
 
-      var stateOptionsList =
-        this.getStates(content.data);
-
-      debugger;
-
-      var itemCount = content.data.length;
+      var items = this.sortAndFilterItems(content.data);
+      var itemCount = items.length;
       var pageCount = Math.ceil(itemCount / maxItems);
+      items = items.slice(pageStart, pageStart +maxItems);
 
       return (
         <div>
@@ -40,7 +43,7 @@ class Finder extends React.Component {
 
           <div class="panel panel-default">
             <div class="panel-heading">
-              <SearchPanel />
+              <SearchPanel search={searchCallback}/>
             </div>
             <div class="panel-body">
               <table class="table">
@@ -50,16 +53,26 @@ class Finder extends React.Component {
                     <th align="left">City</th>
                     <th align="left">
                       <label for="state" style={{ display: 'inline' }}>State</label>
-                      <select name="state" id="stateDropdown" style={{ marginLeft: '5px' }}>
-                        {stateOptionsList}
+                      <select 
+                        id="stateDropdown" 
+                        dir="ltl" 
+                        name="state" 
+                        onChange={this.stateChanged.bind(this)}
+                        style={{ marginLeft: '5px' }}>
+                          <option style={{textAlign:'right'}} value="All">All</option>
+                          {stateOptionsList}
                       </select>
                     </th>
                     <th align="left">Phone</th>
                     <th align="left">
                       <label for="genre" style={{ display: 'inline' }}>Genre</label>
-                      <select name="genre" id="genreDropdown" style={{ marginLeft: '5px' }}>
-                        <option value="">American,Contemporary</option>
-                        <option value="">Cafe,Italian,Bistro,Contemporary,Vegetarian</option>
+                      <select 
+                        id="genreDropdown" 
+                        onChange={this.genreChanged.bind(this)}
+                        name="genre" 
+                        style={{ marginLeft: '5px' }} >
+                        <option style={{textAlign:'right'}} value="">All</option>
+                        {genreOptionsList}
                       </select>
                     </th>
                   </tr>
@@ -83,7 +96,7 @@ class Finder extends React.Component {
             currentPage={currentPage}
             maxItems={maxItems}
             pageCount={pageCount}
-            setPage={callback}
+            setPage={setPageCallback}
           />
         </div>
       );
@@ -96,12 +109,31 @@ class Finder extends React.Component {
     }
   }
 
+  search(text) {
+    this.setState({searchText:text});
+    this.forceUpdate();
+  }
+  
+  stateChanged(event){
+    var currentState = event.target.value;
+    this.setState({selectedState: currentState});
+    this.setState({currentPage:1});
+  }
+
+  genreChanged(event){
+    var currentGenre = event.target.value;
+    this.setState({selectedGenre: currentGenre});
+    this.setState({currentPage:1});
+  }
+
 
   setPage(event) {
 
     var selected = event.target.innerText;
-    var content = this.state.data;
-    var itemCount = content.data.length;
+    var content = this.state.data.data;
+    content = this.sortAndFilterItems(content);
+
+    var itemCount = content.length;
     var currentPage = this.state.currentPage;
     var maxItems = this.state.maxItems;
     var pageCount = Math.ceil(itemCount / maxItems);
@@ -128,24 +160,56 @@ class Finder extends React.Component {
 
 
   componentDidMount() {
+    this.refreshData();
+  }
+
+  refreshData(){
     fetch('/restaurantData.json')
       .then(res => res.json())
       .then((data) => {
         this.setState({ data })
       })
       .catch(console.log)
+
   }
 
-  sortData(items) {
+  sortAndFilterItems(items) {
 
-    return items
-      .sort((a, b) => a.name + a.city + a.state > b.name + b.city + b.state ? 1 : -1)
-      .map((item, index) =>
+    var searchText = this.state.searchText;
+    var stateSelected = this.state.selectedState;
+    var genreSelected = this.state.selectedGenre;
+
+    //Sort by name, then city, then state
+    items.sort((a, b) => a.name + a.city + a.state > b.name + b.city + b.state ? 1 : -1)
+
+    var filteredItems = items;
+
+    //filter for search box
+    if(searchText !== ""){
+      filteredItems = filteredItems.filter((item) => 
+        item.name.toLowerCase().includes(searchText.toLowerCase()) ||
+        item.city.toLowerCase().includes(searchText.toLowerCase) ||
+        item.genre.toLowerCase().includes(searchText.toLowerCase()));
+    }
+
+    //filter for selected State.
+    if(stateSelected !== undefined && stateSelected !=="All"){
+      filteredItems = filteredItems.filter((item) => item.state == stateSelected);
+    }
+
+    //filter for genre
+    if(genreSelected !== undefined && genreSelected !== "All") {
+      filteredItems = filteredItems.filter((item) => item.genre.includes(genreSelected));
+    }
+
+
+    return filteredItems
+      .map((item) =>
         <tr>
           <td>{item.name}</td>
           <td>{item.city}</td>
           <td>{item.state}</td>
-          <td>{item.telephone}</td>
+          <td><a href={'tel:' + item.telephone}>{item.telephone}</a></td>
           <td>{item.genre}</td>
         </tr>
       );
@@ -156,23 +220,54 @@ class Finder extends React.Component {
     var filteredStates = this.filterStates(items);
     return filteredStates
       .sort((a, b) => a.state > b.state ? 1 : -1)
-      .map((item, index) =>
-        <option value="">{item.state}</option>
+      .map((item) =>
+        <option value={item.state}>{item.state}</option>
       );
   }
 
+
+
   filterStates(items) {
     var newList = [];
-    for (var lp = 0; lp < items.length; lp++) {
-      var state = items[lp].state;
-      if (!newList.some(item => item.state == state)) {
-        newList = newList.concat(items[lp]);
+    // eslint-disable-next-line
+    items.map(item => {
+      var currentState = item.state;
+      if (!newList.some(item => item.state === currentState )) {
+        newList = newList.concat(item);
       }
-    }
-    debugger;
+    })
+
     return newList;
   }
 
+  getGenres(items) {
+    var newList = [];
+    var tokenList = [];
+
+    //Build tokens
+    // eslint-disable-next-line
+    items.map(item => {
+      var genre = item.genre;
+      var tokens = genre.split(",");
+    // eslint-disable-next-line
+      tokens.map((token) =>{
+        if(!tokenList.includes(token)){
+          tokenList = tokenList.concat(token);
+        }
+      })
+    });
+
+    tokenList.sort();
+
+    // eslint-disable-next-line
+    tokenList.map(token => {
+      newList = newList.concat(
+        <option>{token}</option>
+      )
+    });
+
+    return newList;
+  }
 
 }
 
--- restaurants/src/components/SearchPanel.js
+++ restaurants/src/components/SearchPanel.js
@@ -4,15 +4,32 @@ class SearchPanel extends React.Component {
 
     render() {
         return (
-            <div class="float-right" style={{padding:'10px'}}>
-                <input />
-                <i 
-                class="fa fa-search fa-lg" 
-                style={{margin:'5px'}}
-                aria-hidden="true">
+            <div class="float-right" style={{ padding: '10px' }}>
+                <input
+                    type="text"
+                    id="searchBox"
+                    onBlur={this.search.bind(this)}
+                    onKeyUp={this.onKeyUpValue.bind(this)}
+                />
+                <i
+                    onClick={this.search.bind(this)}
+                    class="fa fa-search fa-lg"
+                    style={{ margin: '5px' }}
+                    aria-hidden="true">
                 </i>
             </div>
-            );
+        );
+    }
+
+    onKeyUpValue(event) {
+        if (event.code === 'Enter') {
+            this.search();
+        }
+    }
+
+    search() {
+        var text = document.getElementById("searchBox").value;
+        this.props.search(text);
     }
 }
 
